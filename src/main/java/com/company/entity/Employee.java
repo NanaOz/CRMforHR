334f798245f767be0f27a76cfe71f59173d36ec8
Alina <alinavik93@gmail.com>
2022-01-23 04:26:34 +0500

    с исп

--- src/main/java/com/company/entity/Employee.java
+++ src/main/java/com/company/entity/Employee.java
@@ -1,64 +1,57 @@
 package com.company.entity;
-
 import javax.persistence.*;
-
 @Entity
 @Table(name = "employee")
 public class Employee {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
     private Long id;
-
     @OneToOne(optional = false, cascade = CascadeType.ALL)
-    @JoinColumn(name = "userid")
+    @MapsId
+    @JoinColumn(name = "id")
     private User user;
-
     @ManyToOne(optional = false, cascade = CascadeType.ALL)
     @JoinColumn(name = "post_id")
     private Post post;
-
     @ManyToOne(optional = false, cascade = CascadeType.ALL)
     @JoinColumn(name = "status_id")
     private Status status;
- public Employee () {}
-    public Status getStatus() {
-        return status;
-    }
-
-    public void setStatus(Status status) {
-        this.status = status;
-    }
-
-    public Post getPost() {
-        return post;
-    }
-
-    public void setPost(Post post) {
-        this.post = post;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
+    @OneToOne(mappedBy = "employee", cascade = CascadeType.ALL, optional = false, fetch = FetchType.LAZY)
+//    @OneToOne(fetch = FetchType.LAZY)
+    private EmployeeIT employeeIT;
 
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
+    public Employee () {}
+    public Status getStatus() {
+        return status;
+    }
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+    public Post getPost() {
+        return post;
+    }
+    public void setPost(Post post) {
+        this.post = post;
+    }
+    public User getUser() {
+        return user;
+    }
+    public void setUser(User user) {
+        this.user = user;
+    }
+    public EmployeeIT getEmployeeIT() {return employeeIT;}
+    public void setEmployeeIT(EmployeeIT employeeIT) {this.employeeIT = employeeIT;}
 
     @Override
     public String toString() {
         return "Employee{" +
-                "id=" + id +
-                ", user=" + user +
+                "id=" + user.getId() +
+                "user=" + user +
                 ", post=" + post +
                 ", status=" + status +
                 '}';
--- src/main/java/com/company/entity/EmployeeIT.java
+++ src/main/java/com/company/entity/EmployeeIT.java
@@ -1,47 +1,40 @@
 package com.company.entity;
-
 import org.hibernate.annotations.Type;
-
 import javax.persistence.*;
-
+import java.util.ArrayList;
+import java.util.Collection;
 @Entity
 @Table(name = "EmployeeIT")
 public class EmployeeIT {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
     private Long id;
-
     @Lob @Type(type = "org.hibernate.type.TextType")
-    @Column(name = "project", nullable = false)
+    @Column(name = "project")
     private String project;
-
-    @ManyToOne(optional = false, cascade = CascadeType.ALL)
-    @JoinColumn(name = "employee_id")
-    private Employee employeeId;
-
-    public Employee getEmployeeId() {
-        return employeeId;
-    }
-
-    public void setEmployeeId(Employee employeeId) {
-        this.employeeId = employeeId;
-    }
-
-
+    @OneToOne(optional = false, cascade = CascadeType.ALL)
+    @MapsId
+    @JoinColumn(name = "id")
+    private Employee employee;
+    public Long getId() {return id;}
+    public void setId(Long id) {this.id = id;}
+    public Employee getEmployee() {return employee;}
+    public void setEmployee(Employee employee) {this.employee = employee;}
     public String getProject() {
         return project;
     }
-
     public void setProject(String project) {
         this.project = project;
     }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
+//    @OneToMany(mappedBy = "employeeIT", cascade = CascadeType.ALL, orphanRemoval = true)
+//
+//    private Collection<EmployeeITTag> employeeITTags = new ArrayList<>();
+//
+//    public Collection<EmployeeITTag> getEmployeeITTags() {
+//        return employeeITTags;
+//    }
+//
+//    public void setEmployeeITTags(Collection<EmployeeITTag> employeeITTags) {
+//        this.employeeITTags = employeeITTags;
+//    }
+//
 }
