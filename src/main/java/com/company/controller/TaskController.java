1d0fd3a4b5a021ad048a53dad5ef756d2d71f544
Alina <alinavik93@gmail.com>
2022-01-25 01:46:27 +0500

    вью "создать задачу" подключены фио исполнителя и лида

--- src/main/java/com/company/controller/TaskController.java
+++ src/main/java/com/company/controller/TaskController.java
@@ -1,8 +1,7 @@
 package com.company.controller;
 
-import com.company.repository.EmployeeRepository;
-import com.company.repository.TaskRepository;
-import com.company.repository.UserRepository;
+import com.company.repository.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,24 +9,36 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class TaskController {
 
-    private final TaskRepository taskRepository;
-    private final UserRepository userRepository;
     private final EmployeeRepository employeeRepository;
+    private final CandidateRepository candidateRepository;
+    private final EmployeeITRepository employeeITRepository;
+    private final EmployeeHrRepository employeeHrRepository;
+    private final UserRepository userRepository;
+    private final TaskRepository taskRepository;
 
-    public TaskController(TaskRepository taskRepository, UserRepository userRepository, EmployeeRepository employeeRepository) {
-        this.taskRepository = taskRepository;
-        this.userRepository = userRepository;
+    @Autowired
+    public TaskController(EmployeeRepository employeeRepository, CandidateRepository candidateRepository, EmployeeITRepository employeeITRepository, EmployeeHrRepository employeeHrRepository, UserRepository userRepository, TaskRepository taskRepository) {
         this.employeeRepository = employeeRepository;
+        this.candidateRepository = candidateRepository;
+        this.employeeITRepository = employeeITRepository;
+        this.employeeHrRepository = employeeHrRepository;
+        this.userRepository = userRepository;
+        this.taskRepository = taskRepository;
     }
 
 
     @GetMapping("/task")
     public String showTaskList(Model model) {
         model.addAttribute("tasks", taskRepository.findAll());
+        model.addAttribute("employees", employeeRepository.findAll());
+        model.addAttribute("candidatess", candidateRepository.findAll());
+        model.addAttribute("employeesIt", employeeITRepository.findAll());
+        model.addAttribute("employeesHr", employeeHrRepository.findAll());
         model.addAttribute("users", userRepository.findAll());
-        model.addAttribute("employee", employeeRepository.findAll());
         return "task";
     }
+
+
 
 //    @GetMapping("/close")
 //    public String closeInfoForm(Model model) {
