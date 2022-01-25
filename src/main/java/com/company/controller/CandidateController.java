532bd16ca608c4639e95ab48304323b8d328a750
Alina <alinavik93@gmail.com>
2022-01-25 18:23:35 +0500

    вью инфо

--- src/main/java/com/company/controller/CandidateController.java
+++ src/main/java/com/company/controller/CandidateController.java
@@ -1,11 +1,14 @@
 package com.company.controller;
 
+import com.company.entity.Candidate;
+import com.company.entity.Employee;
 import com.company.repository.CandidateRepository;
 import com.company.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
@@ -28,5 +31,17 @@ public class CandidateController {
     public String showCandidateList(Model model) {
         model.addAttribute("candidates", candidateRepository.findAll());
         return "candidate";
+    }
+
+    @GetMapping("/infocand/{id}")
+    public String showInfoForm(@PathVariable("id") long id, Model model) {
+        Candidate candidate = candidateRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+        model.addAttribute("candidates", candidate);
+        return "popup-infocand";
+    }
+
+    @GetMapping("/closex")
+    public String closeInfoForm(Model model) {
+        return "redirect:/candidate#tab_candidate";
     }
 }
