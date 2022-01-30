566bcdaca1a66a2ff4c93d005f921479a7946a7e
Бажен <asbszhen.ru@gmail.com>
2022-01-31 02:55:47 +0700

    -

--- src/main/java/logika/Logics.java
+++ src/main/java/logika/Logics.java
@@ -1,24 +1,45 @@
 package logika;
 
+import org.hibernate.mapping.Map;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Logics {
 
+    private ArrayList<Long> positions;
     //поля
-    ArrayList<String> tags;
-    ArrayList<Long> idUsers;
+    private ArrayList<String> tags;
+    private HashMap<Long, Long> idUsers;
 
     //конструктор
-    public Logics(ArrayList<String> tags, ArrayList<Long> idUsers) {
+    public Logics(ArrayList<String> tags, HashMap<Long, Long> idUsers, ArrayList<Long> positions) {
         this.tags = tags;
         this.idUsers = idUsers;
+        this.positions=positions;
     }
 
-    //метод сортировки
-    private ArrayList<Long> sort() {
-        ArrayList<Long> sortIdUsers = new ArrayList<>();
+    //фильтр по тегам
+    private ArrayList<Long> filter() {
 
+        ArrayList<Long> filterIdUsers = new ArrayList<>();
 
-        return null;
+        for (int i = 0; i < idUsers.size(); i++) { //для каждого пользователя проверяем совпадене тегов
+
+            // контроллер дает arraylist тегов конкретно i-го пользователя
+            ArrayList<String> userTags;
+
+            int flag = 1;
+            for (int j = 0; j < tags.size(); j++) { // для каждого выбранного тега проверяем наличие тега у пользователя
+                for (int k = 0; k < userTags.size(); k++) {
+
+                    if(tags.get(j)!= userTags.get(k)){flag=0;}
+                }
+            }
+
+            if(flag==0){ filterIdUsers.add(idUsers.get(i));} //если у нас выбранный тег есть у пользователя, то добавляем его в список фильтрованных
+        }
+
+        return filterIdUsers;
     }
 }
